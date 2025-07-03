f383cd488f1f526187a2d7c7966f664ab2fc45fb
genarohp <genarohp7@gamil.com>
2025-07-03 15:21:04 -0600

    create cards with javascript

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,71 @@ let name = document.querySelector(".intro__profile-title");
 let job = document.querySelector(".intro__profile-activity");
 let form = document.querySelector(".popup__form");
 
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+const cardContainer = document.querySelector(".grid");
+
+function createCard(name, link) {
+  const card = document.createElement("div");
+  card.classList.add("grid__item");
+
+  const image = document.createElement("img");
+  image.classList.add("grid__item-img");
+  image.src = link;
+  image.alt = name;
+
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("grid__item-text");
+
+  const title = document.createElement("h2");
+  title.classList.add("grid__item-city");
+  title.textContent = name;
+
+  const icon = document.createElement("span");
+  icon.classList.add("fa-regular", "fa-heart", "grid__item-icon");
+
+  const deleteButton = document.createElement("span");
+  deleteButton.classList.add("fa-solid", "fa-trash", "grid__item-delete");
+  card.appendChild(deleteButton);
+
+  textContainer.appendChild(title);
+  textContainer.appendChild(icon);
+  card.appendChild(image);
+  card.appendChild(textContainer);
+
+  return card;
+}
+
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData.name, cardData.link);
+  cardContainer.appendChild(cardElement);
+});
+
 function openpopup() {
   popup.classList.add("popup_opened");
   nameInput.value = "";
